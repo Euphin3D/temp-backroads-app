315520ce37532a5381c7a1b6d41f60d5ac7d08f7
Euphin3D <123968317+Euphin3D@users.noreply.github.com>
2024-11-23 02:55:22 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { footerLink, socialLinks } from "../data";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {footerLink.map((footerLinks) => {
+          const { id, href, text } = footerLinks;
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((socialLink) => {
+          const { id, href, icon } = socialLink;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
